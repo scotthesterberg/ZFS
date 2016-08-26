0458d38ea39b92823036e632b231fff43269effe
Scott Hesterberg <s.c.hesterberg@gmail.com>
2016-08-26 11:41:06 -0400

    fixed messed up quoting in else of finding common snaps

--- zfs_backup.sh
+++ zfs_backup.sh
@@ -104,7 +104,8 @@ if [[ ! -s /tmp/ssh_std_err && ! -s /tmp/zfs_list_err ]]; then
 else
 	#MAIL="ZFS list $store_server snapshots FAILED! Error:"
 	#/bin/printf "$MAIL \n ssh output:\n $( /bin/cat /tmp/ssh_std_err) \n\
-	zfs list output $(/bin/cat /tmp/zfs_list_err)" \
+	
+	zfs list output "$(/bin/cat /tmp/zfs_list_err)" \
 	| /bin/mail -s "ZFS list $store_server snapshots FAILED!" $email
 	
 	Mail "ZFS list $store_server snapshots FAILED"'!' "ZFS list $store_server snapshots FAILED! Error: \n ssh output:\n $( /bin/cat /tmp/ssh_std_err) \n\
@@ -118,7 +119,7 @@ fi
 if [ ! -z $common_snap ]; then
 	#this sends the incrementals of all snapshots created since the last snapshot send to the backup server
 	#ouput of the zfs send and zfs receive commands is saved in temporary txt files to be sent to user
-	#save the exit status of the two sides of pipe in variables and add them for a exit status total`
+	#save the exit status of the two sides of pipe in variables and add them for a exit status total
 	/bin/ssh -i $ssh_backup_key $store_server /sbin/zfs send -vI $store_pool_name/$store_fileshare_name@$common_snap $store_pool_name/$store_fileshare_name@$latest_snap 2> /tmp/zfs_send_tmp.txt\
 	| /sbin/zfs receive -vF $backup_pool_name/$store_backup_fileshare &> /tmp/zfs_receive_tmp.txt;\
 	pipe1=${PIPESTATUS[0]} pipe2=${PIPESTATUS[1]} 
