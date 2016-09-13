10e75dbd971a370824f09a6a2bef89f494580904
Scott Hesterberg <s.c.hesterberg@gmail.com>
2016-09-13 09:40:51 -0400

    got rid of un-needed tr (translates) and used " with echo instead

--- zfs_backup.sh
+++ zfs_backup.sh
@@ -117,14 +117,14 @@ FindCommonSnapshot(){
 	#test to see if we were successful in listing snapshots by checking that the error files don't exist and have a size greater than zero
 	if [[ -z local_snaps && -z remote_snaps ]]; then
 		#find common snapshot on remote and local servers
-		local common_snaps=$(echo ${local_snaps[@]} ${remote_snaps[@]} | tr ' ' '\n' | sort | uniq -d)
+		local common_snaps=$(echo "${local_snaps[@]}" "${remote_snaps[@]}" | sort | uniq -d)
 		if [[ -z common_snaps ]]; then
 			echo "Failed to find common snapshot. This is bad as it means an incremental backup cannot be performed."
 			echo "Please transfer a new common snapshot."
 			return 2
 		else
 			#sort snapshots from previous command by date and add latest common snap to common_snap variable
-			local common_snap=$(echo ${common_snaps[*]} | tr " " "\n" | grep $(echo ${common_snaps[*]} | tr " " "\n" | /bin/cut -d "-" -f4-6 | /bin/sort | /bin/tail -n 1))
+			local common_snap=$(echo ${common_snaps[*]} | tr " " "\n" | grep $(echo ${common_snaps[*]} | tr " " "\n" | /bin/cut -d "-" -f4-6 | /bin/sort | /bin/tail -n 1) | /bin/tail -n 1)
 			
 			RETVAL=$common_snap
 		fi
