d8c8454020fd1ebc6bec753d22d47f3892329d39
Scott Hesterberg <s.c.hesterberg@gmail.com>
2016-09-13 09:35:08 -0400

    removed un-needed sort command from listing of local snaps

--- zfs_backup.sh
+++ zfs_backup.sh
@@ -62,7 +62,7 @@ ListLocalSnapshots(){
 		local snapshot_location=$1
 	fi
 	
-	local local_snaps=$(/sbin/zfs list -Hr -t snap $snapshot_location 2> /tmp/zfs_list_err | /bin/awk '{print $1}' | /bin/awk -F @ '{print $2}' | /bin/sort -r)
+	local local_snaps=$(/sbin/zfs list -Hr -t snap $snapshot_location 2> /tmp/zfs_list_err | /bin/awk '{print $1}' | /bin/awk -F @ '{print $2}')
 	
 	RETVAL=$local_snaps
 	if [ -s /tmp/zfs_list_err ]; then
