cce770c35764c3933c505c6d37b250ede654ddad
Scott Hesterberg <s.c.hesterberg@gmail.com>
2016-09-13 21:30:10 -0400

    many fixes to sendsnapshots function

--- zfs_backup.sh
+++ zfs_backup.sh
@@ -138,12 +138,27 @@ FindCommonSnapshot(){
 }
 
 SendSnapshots(){
+	#location of ssh key to used for authentication
+	local ssh_key=$1
+	#user to authenticate as
+	local user=$2
+	#ip or dns of remote server
+	local remote_server=$3
+	#remote filesystem in format of "zfs_pool_name/zfs_filesystem_name"
+	local remote_fs=$4
+	#local filesystem in format of "zfs_pool_name/zfs_filesystem_name"
+	local local_fs=$5
+	#common snapshot"
+	local common_snap=$6
+	#latest snapshot"
+	local latest_snap=$7
+	
 	if [ ! -z $common_snap ]; then
 		#this sends the incrementals of all snapshots created since the last snapshot send to the backup server
 		#ouput of the zfs send and zfs receive commands is saved in temporary txt files to be sent to user
 		#save the exit status of the two sides of pipe in variables and add them for a exit status total
-		/bin/ssh -i $ssh_backup_key $store_server /sbin/zfs send -vI $store_pool_name/$store_fileshare_name@$common_snap $store_pool_name/$store_fileshare_name@$latest_snap 2> /tmp/zfs_send_tmp.txt\
-		| /sbin/zfs receive -vF $backup_pool_name/$store_backup_fileshare &> /tmp/zfs_receive_tmp.txt;\
+		/bin/ssh -i $ssh_key $user@$remote_server /sbin/zfs send -vI $remote_fs@$common_snap $remote_fs@$latest_snap 2> /tmp/zfs_send_tmp.txt\
+		| /sbin/zfs receive -vF $local_fs &> /tmp/zfs_receive_tmp.txt;\
 		pipe1=${PIPESTATUS[0]} pipe2=${PIPESTATUS[1]} 
 		total_err=$(($pipe1+$pipe2))
 		#tests if zfs send worked successfully, if not send email with exit statuses
